398709cd9f670350f2452ed085fffd6158a8c5a6
John Doeirma <irmaaa96@hotmail.com>
2021-04-18 20:19:16 +0200

    make it better

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,17 @@
 import React from "react";
 import { useAuth } from "../contexts/AuthContext";
-
+ 
 const Home = () => {
-  const { currentUser } = useAuth();
-  return (
+ const { currentUser } = useAuth();
+ 
+ return (
     <div>
-      <p class="Welcome">Welcome</p>
-      <p class="email">
-        {currentUser
-          ? `You are logged as ${(
-              <strong>
-                {currentUser.displayName
-                  ? currentUser.displayName
-                  : currentUser.email}
-              </strong>
-            )}`
-          : "welcome HERE"}
-      </p>
+        <p className="Welcome">Welcome</p>
+        <p className="email">
+            You are logged in as <strong>{currentUser.email}</strong>
+        </p>
     </div>
   );
 };
-
+ 
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,50 +1,51 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
-import { NavLink, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-
+ 
 const NavBar = () => {
-  const { currentUser } = useAuth();
-
-  return (
+ const { currentUser } = useAuth();
+ 
+ return (
     <>
       <Navbar bg="dark" variant="dark">
-        <Container>
-          <Link to="/" className="navbar-brand">
-            Photo review
+          <Container>
+            <Link to="/" className="navbar-brand">
+              Photo review
+            </Link>
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Link to="/albums" className="nav-link">
+              Albums
+            </Link>
+            {currentUser ? (
+            <NavDropdown
+              title={currentUser.displayName || currentUser.email}
+              id="basic-nav-dropdown"
+            >
+            <Link to="/update-profile" className="dropdown-item">
+              Update Profile
+            </Link>
+            <NavDropdown.Divider />
+        
+            <Link to="/logout" className="dropdown-item">
+              Log Out
+            </Link>
+            </NavDropdown>
+        ) : (
+          <Link to="/login" className="nav-link">
+            Login
           </Link>
-
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Link href="/albums" className="nav-link">
-                Albums
-              </Nav.Link>
-              {currentUser ? (
-                <NavDropdown
-                  title={currentUser.displayName || currentUser.email}
-                  id="basic-nav-dropdown"
-                >
-                  <NavLink to="/update-profile" className="dropdown-item">
-                    Update Profile
-                  </NavLink>
-                  <NavDropdown.Divider />
-                  <NavLink to="/logout" className="dropdown-item">
-                    Log Out
-                  </NavLink>
-                </NavDropdown>
-              ) : (
-                <NavLink to="/login" className="nav-link">
-                  Login
-                </NavLink>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Container></Container>
-    </>
+        )}
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  <Container></Container>
+  </>
   );
 };
-
+ 
 export default NavBar;
